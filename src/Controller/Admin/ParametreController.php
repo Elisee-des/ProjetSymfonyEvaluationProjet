6cd657c86d62be681bf86b3050f146e0c7af543c
Elisee-des <esabidani@gmail.com>
2022-07-27 20:08:45 +0000

    Gestion des parametre admin avec sucees

--- src/Controller/Admin/ParametreController.php
+++ src/Controller/Admin/ParametreController.php
@@ -4,10 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use App\Form\ModifAccesType;
+use App\Form\ModifPasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin', name: 'parametre_')]
@@ -50,6 +52,40 @@ class ParametreController extends AbstractController
         }
 
         return $this->render('admin/parametre/modificaficationAcces.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/parametre/modification/mot-de-passe', name: 'modification_mot_de_passe')]
+    public function modifPassword(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    {
+        /**
+         * @var User
+        */
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(ModifPasswordType::class, $user);
+
+        $formaulaire = $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $formaulaire->get("password")->getData();
+            $passwordhash = $userPasswordHasherInterface->hashPassword($user, $password);
+            $user->setPassword($passwordhash);
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash(
+               'success',
+               'Votre mot de passe a ete modifier avec success'
+            );
+
+            return $this->redirectToRoute('parametre_home');
+        }
+
+        return $this->render('admin/parametre/modifiacationPassword.html.twig', [
             'form' => $form->createView()
         ]);
     }
