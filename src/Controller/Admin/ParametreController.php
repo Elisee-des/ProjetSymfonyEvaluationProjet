2f558dbff3564fa0cc03ec1aff5271264080b764
Elisee-des <esabidani@gmail.com>
2022-07-27 17:31:57 +0000

    ok pour Modification des acces

--- src/Controller/Admin/ParametreController.php
+++ src/Controller/Admin/ParametreController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\ModifAccesType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,9 +20,37 @@ class ParametreController extends AbstractController
     }
 
     #[Route('/parametre/modification/acces', name: 'modification_acces')]
-    public function modificationAcces(): Response
+    public function modificationAcces(Request $request, EntityManagerInterface $em): Response
     {
+        /**
+         * @var User
+         */
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(ModifAccesType::class, $user);
+
+        $formaulaire = $form->handleRequest($request);
         
-        return $this->render('admin/parametre/modificaficationAcces.html.twig');
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $nomPrenom = $request->get("modif_acces")["nomPrenom"]; 
+
+            $user->setNomPrenom($nomPrenom);
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash(
+               'success',
+               'Votre nom et prenom a ete modifier'
+            );
+
+            return $this->redirectToRoute('parametre_home');
+        }
+
+        return $this->render('admin/parametre/modificaficationAcces.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
